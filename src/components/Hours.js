0c416c944198909d5e35e94fc6f92f115831839f
DavidGuidoR <131314538+DavidGuidoR@users.noreply.github.com>
2024-08-28 21:13:34 +0000

    cambios en el deploy de azure y correccion de dia en Hours

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,10 +1,18 @@
-// Placeholder to host a component
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Hours() {
-  const today = new Date();
-  const dayOfWeek = today.getDay();
+  const [dayOfWeek, setDayOfWeek] = useState(null);
   const days = ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"];
+
+  useEffect(() => {
+    const today = new Date();
+    setDayOfWeek(today.getDay());
+  }, []);
+
+  if (dayOfWeek === null) {
+    return null; // O un spinner de carga, si prefieres
+  }
+
   const isWeekend = dayOfWeek === 0 || dayOfWeek === 6; // 0 es domingo y 6 es sábado
 
   return (
